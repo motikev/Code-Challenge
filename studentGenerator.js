95f17821de548cea955b79b93c6ec16aa7a02150
motikev <kmotties@gmail.com>
2023-06-18 13:14:51 +0300

    added a JS code to generate student grades

---
+++ studentGenerator.js
@@ -0,0 +1,20 @@
+let studentMarks = 10;
+
+if (studentMarks >= 79 && studentMarks <= 100){
+    console.log ("You got an A")
+}
+ else if (studentMarks > 60 && studentMarks<= 79){
+    console.log("B")
+ }
+ else if(studentMarks > 49 && studentMarks <= 59){
+    console.log("C")
+ }
+ else if(studentMarks > 40 && studentMarks <= 49){
+    console.log("D")
+ }
+ else if(studentMarks > 0 && studentMarks <= 40){
+    console.log("E")
+ }
+ else{
+    console.log("Invalid")
+ }
