e8247167fd7d0bb08f8e08c8795010efe5436683
motikev <kmotties@gmail.com>
2023-06-20 16:01:47 +0300

    completed

--- studentGenerator.js
+++ studentGenerator.js
@@ -1,20 +1,21 @@
-let studentMarks = 10;
+let studentMarks = prompt("enter your marks")
+
 
 if (studentMarks >= 79 && studentMarks <= 100){
-    console.log ("Your score is A")
+    alert ("Your score is A")
 }
- else if (studentMarks > 60 && studentMarks<= 79){
-    console.log("Your score is B")
+ else if (studentMarks >= 60 && studentMarks< 79){
+    alert ("Your score is B")
  }
- else if(studentMarks > 49 && studentMarks <= 59){
-    console.log("Your score is C")
+ else if(studentMarks >= 49 && studentMarks <= 59){
+    alert ("Your score is C")
  }
- else if(studentMarks > 40 && studentMarks <= 49){
-    console.log("Your score is D")
+ else if(studentMarks > 40 && studentMarks < 49){
+    alert("Your score is D")
  }
- else if(studentMarks > 0 && studentMarks <= 40){
-    console.log("Your score E")
+ else if(studentMarks >= 0 && studentMarks <= 40){
+    alert("Your score E")
  }
  else{
-    console.log("Invalid")
+    alert("Invalid")
  }
